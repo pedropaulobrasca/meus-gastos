ccd28a7300d70f42c3620d20eb158774a2a0563b
Pedro Brasca <pedropaulobrasca@gmail.com>
2025-01-26 09:30:36 +0800

    ajusta sw com o novo icon

--- public/sw.js
+++ public/sw.js
@@ -1,41 +1,35 @@
-const CACHE_NAME = 'meus-gastos-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/favicon.ico'
-];
+/* eslint-disable array-callback-return */
+const CACHE_NAME = 'meus-gastos-v1'
+const urlsToCache = ['/', '/index.html', '/manifest.json', '/icon.svg']
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
-});
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  )
+})
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response
+      }
+      return fetch(event.request)
+    })
+  )
+})
 
 self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
+  const cacheWhitelist = [CACHE_NAME]
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
+            return caches.delete(cacheName)
           }
         })
-      );
+      )
     })
-  );
-});
+  )
+})
